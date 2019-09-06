58c64573f63f92e6641e42dbdd403a64c4f61aa4
Serhii Syrotynin <sesyr@telenor.dk>
2019-09-06 15:03:45 +0300

    Initial Commit

---
+++ SecureStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecureStore
+//
+//  Created by Serhii Syrotynin on 9/6/19.
+//  Copyright © 2019 Serhii Syrotynin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
